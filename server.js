bb82cb662d37dd31027d5a6462c8174b2020bd9a
Marko Trebizan <mtrebizan@gmail.com>
2016-07-26 08:02:37 +0200

    add

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+
+//
+'use strict';
 
 let history = [];
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 
-io.on('connection', function(socket){
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
   function send() {
     io.emit('wms send', history);
   }
+  console.log('Client connected');
   send();
-  socket.on('wms send', function(msg){
+  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('wms send', (msg) => {
     while (history.length > 30) {
       history.shift()
     }
@@ -22,7 +35,32 @@ io.on('connection', function(socket){
   });
 });
 
+// setInterval(() => {
+//   wss.clients.forEach((client) => {
+//     client.send(new Date().toTimeString());
+//   });
+// }, 1000);
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+//
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// io.on('connection', function(socket){
+//   function send() {
+//     io.emit('wms send', history);
+//   }
+//   send();
+//   socket.on('wms send', function(msg){
+//     while (history.length > 30) {
+//       history.shift()
+//     }
+//     history.push(msg);
+//     send()
+//   });
+// });
+
+
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
+// });
